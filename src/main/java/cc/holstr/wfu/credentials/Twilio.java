33aeb7ba0495b3556aff5125337b6fdfe34b7821
Jason <jason.p.mcelhenney@gmail.com>
2020-06-08 15:45:30 -0500

    Update Twilio.java
    removed keys.

--- src/main/java/cc/holstr/wfu/credentials/Twilio.java
+++ src/main/java/cc/holstr/wfu/credentials/Twilio.java
@@ -5,6 +5,6 @@ package cc.holstr.wfu.credentials;
  */
 public class Twilio {
 
-	public static final String ACCOUNT_SID = "AC4c5603f878fc21bc02ea4b0cf91c896c";
-	public static final String AUTH_TOKEN = "72de3a30bfebbfd99abd3677949a4f53";
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
 }
